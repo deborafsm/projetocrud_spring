93222eb086fc83031f0093b5568985e4f9513956
Débora Freire <deborafreiresoares@outlook.com>
2025-07-12 21:53:01 -0300

    Initial commit

---
+++ projetocrud/src/main/java/com/dnd/projetocrud/infraestructure/repository/UsuarioRepository.java
@@ -0,0 +1,18 @@
+package com.dnd.projetocrud.infraestructure.repository;
+
+import com.dnd.projetocrud.infraestructure.entitys.Usuario;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+
+
+
+}
---
+++ projetocrud/src/test/java/com/dnd/projetocrud/ProjetocrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.projetocrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetocrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
